c56702cfe30fb0d5c64d9fcafffac2c697c2d9b7
Abdou Anmh <abdou.nazieh717437@gmail.com>
2022-11-30 20:03:59 +0200

    new update to my project

--- ecommerce/assets/js/main.js
+++ ecommerce/assets/js/main.js
@@ -32,13 +32,35 @@
 
 
 /*=============== HOME SWIPER ===============*/
-
+var homeSwiper = new Swiper(".home-swiper",{
+    spaceBetween: 30,
+    loop:'true',
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true
+    },
+});
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
+function scrollHeader(){
+    const header = document.getElementById('header')
+    //when the scroll is greater than 50 viewport , add the scroll-header class to the header tag
+    if(this.scrollY >= 50)
+        header.classList.add('scroll-header');
+    else
+        header.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader);
 
 /*=============== NEW SWIPER ===============*/
-
+var newSwiper = new Swiper(".new-swiper",{
+    spaceBetween: 16,
+    centeredSlides:true,
+    slidesPerView: "auto",
+    loop:'true',
+});
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
